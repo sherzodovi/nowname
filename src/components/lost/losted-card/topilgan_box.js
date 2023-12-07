ba69858d64bc5498435d80e2901371518242407f
ascodev_ <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>
2023-12-07 04:05:23 -0800

    index

--- src/components/lost/losted-card/topilgan_box.js
+++ src/components/lost/losted-card/topilgan_box.js
@@ -1,18 +1,15 @@
-import { useState } from 'react'
+import axios from 'axios'
+import { useEffect, useState } from 'react'
 import { Button, Modal, ModalBody, ModalFooter, ModalHeader } from 'reactstrap'
 import img from '../../../assets/img/theme/team-1-800x800.jpg'
 import './topilgan_box.css'
-import DemoNavbar from 'components/Navbars/DemoNavbar'
-import axios from 'axios'
-
-
 
 function Lostedcard() {
-	axios.get('https://lostfoundapi.pythonanywhere.com/item').then(res => {
-		console.log(res);
-	})
+
+
 	const [modal, setModal] = useState(false)
 	const toggle = () => setModal(!modal)
+
 	return (
 		<>
 			<div class='ccc card-deck'>
@@ -21,39 +18,44 @@ function Lostedcard() {
 					<div class='card-body'>
 						<h5 class='card-title'>Card title</h5>
 						<p class='card-text'>
-							This is a wider card with supporting text below as a natural lead-in
-							to additional content. This content is a little bit longer.
+							This is a wider card with supporting text below as a natural
+							lead-in to additional content. This content is a little bit
+							longer.
 						</p>
-					</div>
-					<div class='card-footer'>
-						<small class='text-muted'>Last updated 3 mins ago</small>
-					</div>
-				</div>
-				<div class='card' onClick={toggle}>
-					<img class='card-img-top' src={img} alt='Card image cap' />
-					<div class='card-body'>
-						<h5 class='card-title'>Card title</h5>
-						<p class='card-text'>
-							This is a wider card with supporting text below as a natural lead-in
-							to additional content. This content is a little bit longer.
-						</p>
-					</div>
-					<div class='card-footer'>
-						<small class='text-muted'>Last updated 3 mins ago</small>
-					</div>
-				</div>
-				<div class='card' onClick={toggle}>
-					<img class='card-img-top' src={img} alt='Card image cap' />
-					<div class='card-body'>
-						<h5 class='card-title'>Card title</h5>
-						<p class='card-text'>
-							This is a wider card with supporting text below as a natural lead-in
-							to additional content. This content is a little bit longer.
-						</p>
-					</div>
-					<div class='card-footer'>
-						<small class='text-muted'>Last updated 3 mins ago</small>
 
+
+
+					</div>
+					<div class='card-footer'>
+						<small class='text-muted'>Last updated 3 mins ago</small>
+					</div>
+				</div>
+				<div class='card' onClick={toggle}>
+					<img class='card-img-top' src={img} alt='Card image cap' />
+					<div class='card-body'>
+						<h5 class='card-title'>Card title</h5>
+						<p class='card-text'>
+							This is a wider card with supporting text below as a natural
+							lead-in to additional content. This content is a little bit
+							longer.
+						</p>
+					</div>
+					<div class='card-footer'>
+						<small class='text-muted'>Last updated 3 mins ago</small>
+					</div>
+				</div>
+				<div class='card' onClick={toggle}>
+					<img class='card-img-top' src={img} alt='Card image cap' />
+					<div class='card-body'>
+						<h5 class='card-title'>Card title</h5>
+						<p class='card-text'>
+							This is a wider card with supporting text below as a natural
+							lead-in to additional content. This content is a little bit
+							longer.
+						</p>
+					</div>
+					<div class='card-footer'>
+						<small class='text-muted'>Last updated 3 mins ago</small>
 					</div>
 				</div>
 
@@ -63,12 +65,10 @@ function Lostedcard() {
 					fullscreen
 					className='w-75 float-right mx-3'
 				>
-					<ModalHeader toggle={toggle}>
-						describtion losted thing
-					</ModalHeader>
+					<ModalHeader toggle={toggle}>describtion losted thing</ModalHeader>
 					<ModalBody className='px-3'>
 						<div className=' y-center'>
-							<img src={img} alt="" className='wd-20 rounded' />
+							<img src={img} alt='' className='wd-20 rounded' />
 							<h4 className='ml-3'>Lorem ipsum dolor sit.</h4>
 						</div>
 						<div className='y-center mt-3 bg-success border-5 px-3 text-wight w-m-content'>
@@ -77,7 +77,8 @@ function Lostedcard() {
 						</div>
 						<div className='mt-4'>
 							<h5>
-								Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iusto iure alias nisi animi eum maiores.
+								Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iusto
+								iure alias nisi animi eum maiores.
 							</h5>
 						</div>
 					</ModalBody>
@@ -92,7 +93,6 @@ function Lostedcard() {
 				</Modal>
 			</div>
 		</>
-
 	)
 }
 
