8b6026942eff1607f6133b4079126db2e02dc0b0
ascodev_ <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>
2023-12-07 06:55:12 -0800

    dfg

--- src/components/Navbars/DemoNavbar.js
+++ src/components/Navbars/DemoNavbar.js
@@ -88,49 +88,6 @@ function DemoNavbar() {
 
 
 
-=======
-				<Button
-					className='btn'
-					color='primary'
-					to='/register-page'
-					id='Register'
-				>
-					<Link to='/register'>Home</Link>
-				</Button>
-				<Button color='success' className='btn'>
-					<Link to='/login'>About</Link>
-				</Button>
-				<UncontrolledDropdown className='me-2' direction='down'>
-					<DropdownToggle caret color='primary' me=''>
-						Dropdown
-					</DropdownToggle>
-					<DropdownMenu>
-						<DropdownItem header>
-							<Link to='/register'>Register</Link>
-						</DropdownItem>
-						<DropdownItem disabled>
-							<Link to='/login'>Lo</Link>
-						</DropdownItem>
-					</DropdownMenu>
-				</UncontrolledDropdown>
-				<button className='nav-btn  nav-close-btn' onClick={showNavbar}>
-					<FaTimes />
-				</button>
-			</nav>
-			<button className='nav-btn' onClick={showNavbar}>
-				<FaBars />
-			</button>
-
-			<UncontrolledDropdown className='me-2' direction='down'>
-				<DropdownToggle caret color='primary' me=''>
-					Dropdown
-				</DropdownToggle>
-				<DropdownMenu>
-					<DropdownItem header>Register</DropdownItem>
-					<DropdownItem disabled>Login</DropdownItem>
-				</DropdownMenu>
-			</UncontrolledDropdown>
-		</header>
 	)
 
 }
